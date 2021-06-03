268ab8b1e14c758bfdebf293838663b6e5c84825
Stephane Duval <sduval@isi-mtl.com>
2021-06-03 09:46:32 -0400

    correction du calcul de l'angle forme par 2 vecteurs

--- Visual Studio/ex trigonometrie/Source.cpp
+++ Visual Studio/ex trigonometrie/Source.cpp
@@ -61,32 +61,59 @@ void quit_SDL() {
 
 
 //	1. hauteur d'une tour
+//	---------------------
 double searchTowerHeight(double horizontalHeadTopAngle, double observerHeight, double distanceToObject) {
 	return observerHeight + distanceToObject * tan(horizontalHeadTopAngle * M_PI / 180);
 }
 
 //	2. angle forme par 2 vecteurs donnes
+//	------------------------------------
 double computeAngle(const Vector& v1, const Vector& v2) {
 	//	calculer le sinus et le cosinus de l'angle (v1, v2)
 	//	calculer un angle avec (par exemple) acos
 	//	en observant le signe du sinus, determiner l'angle
+
+	//	calcul du produit scalaire
+	double dotProduct = v1.x * v2.x + v1.y * v2.y;			//	= ||v1|| * ||v2|| * cos(v1, v2)
+	double normeV1 = sqrt(v1.x * v1.x + v1.y * v1.y);		//	= ||v1||
+	double normeV2 = sqrt(v2.x * v2.x + v2.y * v2.y);		//	= ||v2||
+	double cosinusV1V2 = dotProduct / (normeV1 * normeV2);	//	= cos(v1, v2)
+	double unAngle = acos(cosinusV1V2);						//	cet angle a la meme valeur de cosinus que celle de l'angle (v1, v2)
+	//	ATTENTION : cet angle est compris entre 0 et 180 degres !
+
+	//	pour determiner si l'angle precedent est effectivement l'angle (v1, v2), on test son sinus :
+	double crossProduct = v1.x * v2.y - v1.y * v2.x;	//	= ||v1|| * ||v2|| * sin(v1, v2) 
+
+	if (crossProduct < 0) {		//	l'angle (v1, v2) est compris entre 180 et 360 degres...
+		unAngle = 2 * M_PI - unAngle;
+	}
+
+	return unAngle;
 }
+
+//	3. distance d'un bateau a la cote
+//	---------------------------------
+
 
 //	entry point of application
 int main(int argc, char** argv) {
 	SDL_Renderer* renderer = init_SDL("Trigonometrie");	//	this object will draw in our window
 
 	//	1. height of a tower
+	//	--------------------
 	//double alpha = 30, h = 1.70, d = 500;
 	//double alpha = 60, h = 1.78, d = 15;
 	double alpha = 45, h = 1.80, d = 50;
-
 	cout << searchTowerHeight(alpha, h, d) << endl;
 
-	/*	prepare useful objects here	*/
-
-
-
+	//	2. angle forme par 2 vecteurs donnes
+	//	------------------------------------
+	Color colors[3] = { Color(255, 0, 0, SDL_ALPHA_OPAQUE),
+						Color(0, 255, 0, SDL_ALPHA_OPAQUE),
+						Color(0, 0, 255, SDL_ALPHA_OPAQUE) };
+	Point points[3] = { Point(WIDTH / 2, HEIGHT / 2, true) ,
+						Point(WIDTH * 2 / 3, HEIGHT / 2, true),
+						Point(WIDTH / 2 + 50, HEIGHT / 2 - 150, true) };
 
 	//	*********  //
 	//	main loop  //
@@ -99,6 +126,20 @@ int main(int argc, char** argv) {
 		clearWindow(renderer);
 
 		/*	draw any desired graphical objects here	*/
+		for (int i = 0; i < 3; i++) {
+			points[i].draw(renderer, colors[i], 10);
+		}
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+		SDL_RenderDrawLine(renderer, points[0].x, points[0].y, points[1].x, points[1].y);	//	vector v1
+		SDL_RenderDrawLine(renderer, points[0].x, points[0].y, points[2].x, points[2].y);	//	vector v2
+
+		//	compute and print angle
+		Vector v1(points[0], points[1]);
+		Vector v2(points[0], points[2]);
+		//	invesion des y car l'axe des Y sur l'ecran est dirige vers le bas
+		v1.y = -v1.y;
+		v2.y = -v2.y;
+		cout << computeAngle(v1, v2) * 180 / M_PI << endl;
 
 		//	****************  //
 		//	event management  //
@@ -106,6 +147,9 @@ int main(int argc, char** argv) {
 		SDL_Event event = getNextEvent();
 
 		/*	give event to objects for update if needed here	*/
+		for (int i = 0; i < 3; i++) {
+			points[i].update(event);
+		}
 
 		showRenderingBuffer(renderer);
 
