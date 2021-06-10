b6b0860630f42637daa0bda7f260e82ee5f82131
Stephane Duval <sduval@isi-mtl.com>
2021-06-10 13:06:51 -0400

    exemple pour faire tourner un triangle.

---
+++ Visual Studio/2.rotation triangle/Source.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+using namespace std;
+
+#include <SDL.h>
+
+//	****************  //
+//	window attributs  //
+//	****************  //
+//	- position and size on screen
+constexpr auto POS_X = 1920 + 200, POS_Y = 100;
+constexpr auto WIDTH = 400, HEIGHT = 400;
+
+//	include desired header files for libraries
+#include "../lib_Point/Point.h"
+
+SDL_Renderer* init_SDL(const char* title) {
+#pragma region SDL initialization
+	// SDL initialization
+	SDL_Window* window = NULL;
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		cout << "ERREUR : can't initialize SDL!" << endl;
+		exit(0);
+	}
+
+	//SDL_ShowCursor(SDL_DISABLE);	//	hide mouse cursor
+	SDL_ShowCursor(SDL_ENABLE);	//	show mouse cursor
+
+	//	create the window and its associated renderer
+	window = SDL_CreateWindow(title, POS_X, POS_Y, WIDTH, HEIGHT, 0);
+	return SDL_CreateRenderer(window, 0, 0);
+#pragma endregion
+}
+
+void clearWindow(SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(renderer);
+}
+
+void showRenderingBuffer(SDL_Renderer* renderer) {
+	SDL_RenderPresent(renderer);
+}
+
+SDL_Event getNextEvent() {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	return event;
+}
+
+bool keypressed(SDL_Event& event, char key) {
+	return event.type == SDL_KEYDOWN && event.key.keysym.sym == key;
+}
+
+void quit_SDL() {
+#pragma region SDL quit
+	//	quit SDL
+	SDL_Quit();
+#pragma endregion
+}
+
+void drawTriangle(SDL_Renderer* renderer, Point centre, double alpha) {
+	double angles[] = { 0, 150, 210 };
+	double l = 100;
+
+	SDL_Point points_ecran[4];
+	for (int i = 0; i < 3; i++) {
+		angles[i] *= M_PI / 180;
+		points_ecran[i].x = centre.x + cos(angles[i] + alpha) * l;
+		points_ecran[i].y = centre.y - sin(angles[i] + alpha) * l;
+	}
+	points_ecran[3] = points_ecran[0];
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+	SDL_RenderDrawLines(renderer, points_ecran, 4);
+}
+
+
+//	entry point of application
+int main(int argc, char** argv) {
+	SDL_Renderer* renderer = init_SDL("SLD template");	//	this object will draw in our window
+
+	/*	prepare useful objects here	*/
+	double alpha = 0;
+
+	//	*********  //
+	//	main loop  //
+	//	*********  //
+	bool endOfGame = false;
+	while (!endOfGame) {
+		//	******************************  //
+		//	draw image in rendering buffer  //
+		//	******************************  //
+		clearWindow(renderer);
+
+		/*	draw any desired graphical objects here	*/
+		drawTriangle(renderer, Point(WIDTH / 2, HEIGHT / 2), alpha);
+		//alpha += 2 * M_PI / 10000;
+
+		//	****************  //
+		//	event management  //
+		//	****************  //
+		SDL_Event event = getNextEvent();
+
+		/*	give event to objects for update if needed here	*/
+		if (event.type == SDL_KEYDOWN) {
+			if (event.key.keysym.sym == SDLK_RIGHT) {
+				alpha -= 2 * M_PI / 200;
+			}
+			else if (event.key.keysym.sym == SDLK_LEFT) {
+				alpha += 2 * M_PI / 200;
+			}
+		}
+
+		showRenderingBuffer(renderer);
+
+		endOfGame = keypressed(event, 'q');
+	}
+
+	quit_SDL();
+
+	return 0;
+}
