a9e769eda9e962a1c29b592ba46844c4aa511db3
Stephane Duval <sduval@isi-mtl.com>
2021-09-21 15:36:27 -0400

    ajout d'un label pour chaque noeud d'un BT

--- Visual Studio/A-Star/BT.h
+++ Visual Studio/A-Star/BT.h
@@ -7,17 +7,21 @@ using namespace std;
 
 class BT {
 private:
+	string label;
 	NodeType type;
 
 	IActor* actor;
 	int idAction;
 
 	vector<BT*> children;
+	void print(int offset);
 
 public:
-	BT(IActor* actor, int idAction);
-	BT(NodeType type);
+	BT(string label, IActor* actor, int idAction);
+	BT(string label, NodeType type);
 	void addChild(BT* child);
 	ValueBT eval();
+
+	void print();
 };
 
