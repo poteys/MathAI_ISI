41abafa75be3364bfe061df011cf4394656b11ae
Stephane Duval <sduval@isi-mtl.com>
2021-09-24 08:31:25 -0400

    ajout d'un type aux cellules de grille

--- Visual Studio/A-Star/Cell.h
+++ Visual Studio/A-Star/Cell.h
@@ -1,12 +1,21 @@
 #pragma once
+
+enum CellType {
+	EMPTY, WALL
+};
+
 class Cell {
 private:
 	int row, column;
+	CellType type;
+
 public:
 	Cell(int row, int column);
 
 	int getRow() const;
 	int getCol() const;
+	CellType getType() const;
+	void setType(CellType type);
 
 	double getDistance(Cell* cell) const;
 };
